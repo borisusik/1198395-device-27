9f42bc160ab201e8ce82a109e174f4d64d249e7b
Борис <borisusik@gmail.com>
2020-03-11 12:14:48 +0300

    Подогнал под превьюхи

--- js/contact-modal.js
+++ js/contact-modal.js
@@ -32,14 +32,26 @@ close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
     popup.classList.remove("modal-error");
+    userName.classList.remove("invalid");
+    userEmail.classList.remove("invalid");
+    userMessage.classList.remove("invalid");
 });
 
 form.addEventListener("submit", function (evt) {
-    if (!userName.value || !userEmail.value) {
+    if (!userName.value || !userEmail.value || !userMessage.value) {
         evt.preventDefault();
         popup.classList.remove("modal-error");
         popup.offsetWidth = popup.offsetWidth;
         popup.classList.add("modal-error");
+        if (!userName.value) {
+            userName.classList.add("invalid");
+        }
+        if (!userEmail.value) {
+            userEmail.classList.add("invalid");
+        }
+        if (!userMessage.value) {
+            userMessage.classList.add("invalid");
+        }
     } else {
         if (isStorageSupport) {
             localStorage.setItem("userName", userName.value);
@@ -54,6 +66,27 @@ window.addEventListener("keydown", function (evt) {
         if (popup.classList.contains("modal-show")) {
             popup.classList.remove("modal-show");
             popup.classList.remove("modal-error");
+            userName.classList.remove("invalid");
+            userEmail.classList.remove("invalid");
+            userMessage.classList.remove("invalid");
         }
+    }
+});
+
+userName.addEventListener("focus", function () {
+    if (userName.classList.contains("invalid")) {
+        userName.classList.remove("invalid");
+    }
+});
+
+userEmail.addEventListener("focus", function () {
+    if (userEmail.classList.contains("invalid")) {
+        userEmail.classList.remove("invalid");
+    }
+});
+
+userMessage.addEventListener("focus", function () {
+    if (userMessage.classList.contains("invalid")) {
+        userMessage.classList.remove("invalid");
     }
 });
